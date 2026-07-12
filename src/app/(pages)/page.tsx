d52b781c10788ec19dda1000aed6b4b2f07c4234
codewithtabish <kashisultan099@gmail.com>
2026-07-12 17:22:40 +0500

    responsive one fixed

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,18 +1,7 @@
-import { CallToAction } from '@/components/cta'
 import HeroSection from '@/components/general/(landing)/(hero)/hero-section'
 import APPNavBar from '@/components/general/(landing)/(navbar)/navbar'
-import { CarInspectionDemoSection } from '@/components/general/(landing)/car-inspection-demo'
-import WhyChooseUsTabs from '@/components/general/(landing)/choose-us-section'
 import ServicesSection from '@/components/general/(landing)/services-check'
 import { Container } from '@/components/general/container'
-import { Hero47 } from '@/components/hero47'
-import AccordionCarousel from '@/components/pixel-perfect/accordion-carousel'
-import FeaturedCarsSection from '@/components/pixel-perfect/cover-flow-carousel'
-import SlicedRevealCarousel from '@/components/pixel-perfect/sliced-reveal-carousel'
-import TestimonialsSection from '@/components/testimonials-02'
-import FaqAccordion from '@/components/ui/faq-accordion'
-import { NotchNavbar } from '@/components/ui/notch-navbar'
-import React from 'react'
 
 
 
