0255bd6e2f37b0ccbd24d47a035c6826805ed4a2
codewithtabish <kashisultan099@gmail.com>
2026-07-18 10:25:31 +0500

    added link

--- src/app/(pages)/dashboard/blogs/page.tsx
+++ src/app/(pages)/dashboard/blogs/page.tsx
@@ -9,7 +9,7 @@ const DashboardBlogsPage = () => {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sint delectus, excepturi earum necessitatibus nemo iste voluptatem est nisi animi voluptate quibusdam fuga dignissimos incidunt eligendi voluptatum sit repellat ratione?
         <hr />
         <Link href={'/dashboard/blogs/create'}>
-        Create blog
+        Create blogs
         </Link>
       </Container>
 
